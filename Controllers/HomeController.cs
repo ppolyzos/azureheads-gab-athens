c8cb3a00312c4ad1f852119360992400fb52a1a6
ppolyzos <ppolyzos@gmail.com>
2020-04-18 14:38:27 +0200

    feat(cards): enable speaker cards

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Dynamic;
 using Microsoft.AspNetCore.Mvc;
 using gab_athens.Models;
 
@@ -10,11 +8,45 @@ namespace gab_athens.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private Dictionary<string, string> mappings = new Dictionary<string, string>()
+        {
+            { "chatzipavlis", "antonios-chatzipavlis" } ,
+            { "fintzos", "dimitris-fintzos" } ,
+            { "pantazis", "dimitris-pantazis" } ,
+            { "grammatikos", "george-grammatikos" } ,
+            { "markou", "george-markou" } ,
+            { "kalyva", "georgia-kalyva" } ,
+            { "kavvalakis", "giorgos-kavvalakis" } ,
+            { "pantos", "konstantinos-pantos" } ,
+            { "haggar", "michail-haggar" } ,
+            { "antoniou", "nikolaos-antoniou" } ,
+            { "apostolidis", "pantelis-apostolidis" } ,
+            { "polyzos", "paris-polyzos" } ,
+            { "kappas", "vaggelis-kappas" } ,
+            { "touliatos", "yanni-touliatos" }
+        };
+
+        public IActionResult Index(string speaker)
         {
             //return View("~/Views/Home/Index.cshtml"); // Default for gab-athens-2019
             //return View("~/Views/Ai/Index.cshtml"); // Default for ai-athens-2019
             return View("~/Views/ga-2020/Index.cshtml"); // Default for global-azure-2020
+        }
+
+        [Route("speaker/{speaker}")]
+        public IActionResult Speaker(string speaker)
+        {
+            speaker = speaker.ToLowerInvariant();
+            mappings.TryGetValue(speaker, out var speakerImage);
+            var speakerCard = new ExpandoObject();
+            speakerCard.TryAdd("image", speakerImage ?? "speakers");
+            return View("~/Views/ga-2020/Speaker.cshtml", speakerCard);
+        }
+
+        [Route("{speaker}")]
+        public IActionResult SpeakerIndex(string speaker)
+        {
+            return Speaker(speaker);
         }
 
         public IActionResult Error()
