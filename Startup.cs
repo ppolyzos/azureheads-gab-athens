491e88adadab34facb3f734c9c768160986c36f9
ppolyzos <ppolyzos@gmail.com>
2020-04-25 14:21:31 +0200

    feat(app): enable application insights

--- Startup.cs
+++ Startup.cs
@@ -22,6 +22,7 @@ namespace gab_athens
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
