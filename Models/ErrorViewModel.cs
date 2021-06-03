5fe734d7475d334d17931ff7258f87b5ba2001ab
Paris Polyzos <ppolyzos@gmail.com>
2021-06-03 20:21:19 +0200

    chore: folder restructuring

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EventManagement.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
