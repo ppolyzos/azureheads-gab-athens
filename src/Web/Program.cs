9217e44ac69e108a8e71fae378ee6da20d1001ca
Paris Polyzos <ppolyzos@gmail.com>
2021-06-04 10:42:32 +0200

    feat(app): changes in startup program

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -3,74 +3,55 @@ using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Events;
 
 namespace EventManagement.Web
 {
     public class Program
     {
         public static readonly string Namespace = typeof(Program).Namespace;
-        public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
-        
-        public static int Main(string[] args)
-        {
-            var configuration = GetConfiguration();
-            
-            Log.Logger = CreateSerilogLogger(configuration);
-            
-            try
-            {
-                Log.Information("Configuring web host ({ApplicationContext})...", AppName);
-                var host = BuildWebHost(configuration, args);
 
-                Log.Information("Starting web host ({ApplicationContext})...", AppName);
-                host.Run();
+        public static readonly string AppName =
+            Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
 
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Program terminated unexpectedly ({ApplicationContext})!", AppName);
-                return 1;
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
-        
-        private static IConfiguration GetConfiguration()
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables();
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
 
-            return builder.Build();
-        }
-        
-        private static ILogger CreateSerilogLogger(IConfiguration configuration)
-        {
-            var seqServerUrl = configuration["Serilog:SeqServerUrl"];
-            var logstashUrl = configuration["Serilog:LogstashUrl"];
-            return new LoggerConfiguration()
-                .MinimumLevel.Verbose()
-                .Enrich.WithProperty("ApplicationContext", AppName)
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .WriteTo.Seq(string.IsNullOrWhiteSpace(seqServerUrl) ? "http://seq" : seqServerUrl)
-                .WriteTo.Http(string.IsNullOrWhiteSpace(logstashUrl) ? "http://logstash:8080" : logstashUrl)
-                .ReadFrom.Configuration(configuration)
-                .CreateLogger();
-        }
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((host, builder) =>
+                {
+                    builder.SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{host.HostingEnvironment.EnvironmentName}.json", optional: false,
+                            reloadOnChange: true)
+                        .AddEnvironmentVariables();
+                })
+                .ConfigureWebHostDefaults(webBuilder => webBuilder
+                    .UseStartup<Startup>()
+                    .CaptureStartupErrors(false)
+                    .UseContentRoot(Directory.GetCurrentDirectory()))
+                .UseSerilog((host, builder) =>
+                {
+                    var seqServerUrl = host.Configuration["Serilog:SeqServerUrl"];
+                    var logstashUrl = host.Configuration["Serilog:LogstashUrl"];
 
-        private static IWebHost BuildWebHost(IConfiguration configuration, string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .CaptureStartupErrors(false)
-                .UseStartup<Startup>()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseConfiguration(configuration)
-                .UseSerilog()
-                .Build();
+                    builder.MinimumLevel.Verbose()
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                        .Enrich.WithProperty("ApplicationContext", host.HostingEnvironment.ApplicationName)
+                        .Enrich.FromLogContext()
+                        .WriteTo.Console()
+                        .WriteTo.Seq(string.IsNullOrWhiteSpace(seqServerUrl)
+                            ? "http://seq"
+                            : seqServerUrl)
+                        .WriteTo.Http(string.IsNullOrWhiteSpace(logstashUrl)
+                            ? "http://logstash:8080"
+                            : logstashUrl)
+                        .ReadFrom.Configuration(host.Configuration);
+                });
     }
 }
